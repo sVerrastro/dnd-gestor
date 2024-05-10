0a32f23c1591e2bb02e076bef9bc6348674a56e4
Sergio Verrastro <sergio.09.2005@gmail.com>
2024-05-10 12:53:10 +0200

    java

--- java/src/Elfo.java
+++ java/src/Elfo.java
@@ -2,9 +2,34 @@ import java.util.Vector;
 
 public class Elfo {
     
-    Privilegio privilegio1 = new Privilegio("scurovisione");
-    Privilegio privilegio2 = new Privilegio("astuzia gnomesca");
+    Vector<Privilegio> privilegi = new Vector<>();
+    Vector<String> languages = new Vector<>();
+
+    public Elfo () {
+        loadPrivileges();
+        loadLanguages();
+    }
     
+    public void loadPrivileges() {
+        privilegi.add(new Privilegio("scurovisione"));
+        privilegi.add(new Privilegio("Sensi Acuti"));
+        privilegi.add(new Privilegio("Retaggio Fatato"));
+        privilegi.add(new Privilegio("Trance"));
+    }
 
+    public void loadLanguages() {
+        languages.add("Comune");
+        languages.add("Elfico");
+    }
 
+    private class Subrace {
+    
+        String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        
+    }
 }
